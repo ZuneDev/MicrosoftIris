da71a596a931e83de10be25af711e6f4287897a1
Yoshi Askharoun <jjask7@gmail.com>
2022-07-15 21:00:29 -0500

    Added simple Iris test app

---
+++ Tests/SimpleIrisApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Iris;
+using System;
+using System.IO;
+
+namespace SimpleIrisApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Launching Iris app...");
+
+        Application.Initialize();
+
+        Application.Window.Caption = "Iris app";
+        Application.Window.RequestLoad("clr-res://SimpleIrisApp!MainPage.uix#Frame");
+
+        Application.Run(OnInitialLoadComplete);
+    }
+
+    static void OnInitialLoadComplete(object arg)
+    {
+        Console.WriteLine("Initial load complete");
+    }
+}
