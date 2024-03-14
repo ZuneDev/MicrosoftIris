9cbf962471b1969a6eaae402afbdcaa659b26885
Yoshi Askharoun <jjask7@gmail.com>
2024-03-14 02:45:17 -0500

    Add fonts

---
+++ IrisShell/UI/FontLoader.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Iris;
+
+namespace IrisShell.UI;
+
+public class FontLoader
+{
+    private List<string>? _fonts;
+    private string? _resourceDll;
+    private bool _loaded;
+    private bool _loadQueued;
+
+    public List<string>? Fonts
+    {
+        get => _fonts;
+        set
+        {
+            _fonts = value;
+            QueueFontLoading();
+        }
+    }
+
+    public string Resource
+    {
+        set
+        {
+            _resourceDll = value;
+            QueueFontLoading();
+        }
+    }
+
+    private void QueueFontLoading()
+    {
+        if (_loaded)
+            throw new InvalidOperationException("This is a one trick pony, sorry.");
+        if (_loadQueued)
+            return;
+        Application.DeferredInvoke(LoadFonts, null);
+        _loadQueued = true;
+    }
+
+    private void LoadFonts(object args)
+    {
+        _loadQueued = false;
+        if (_fonts == null || _fonts.Count == 0)
+            return;
+        if (_resourceDll == null)
+            throw new InvalidOperationException("Must specify a Resource to retrieve the fonts from.");
+        foreach (string font in _fonts)
+            MemoryFonts.TryLoadFromResource(_resourceDll, font);
+        _loaded = true;
+    }
+}
---
+++ IrisShell/UI/MemoryFonts.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+
+namespace IrisShell.UI;
+
+internal class MemoryFonts
+{
+    public const uint LOAD_LIBRARY_AS_DATAFILE = 2;
+    public const int RT_DATA = 10;
+
+    public static bool TryLoadFromResource(string resourceDllName, string fontResourceName)
+    {
+        IntPtr instanceHandle = IntPtr.Zero;
+        try
+        {
+            instanceHandle = LoadLibraryEx(resourceDllName, IntPtr.Zero, 2U);
+            if (instanceHandle == IntPtr.Zero)
+                return false;
+            IntPtr resource = FindResource(instanceHandle, fontResourceName, (IntPtr)10);
+            if (resource == IntPtr.Zero)
+                return false;
+            IntPtr resourceHandle = LoadResource(instanceHandle, resource);
+            return !(resourceHandle == IntPtr.Zero) && !(AddFontMemResourceEx(LockResource(resourceHandle), SizeofResource(instanceHandle, resource), IntPtr.Zero, out uint _) == IntPtr.Zero);
+        }
+        finally
+        {
+            if (instanceHandle != IntPtr.Zero)
+                FreeLibrary(instanceHandle);
+        }
+    }
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr LoadLibraryEx(string moduleName, IntPtr reserved, uint flags);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+    public static extern bool FreeLibrary(IntPtr instanceHandle);
+
+    [DllImport("kernel32.dll", EntryPoint = "FindResourceW", CharSet = CharSet.Unicode)]
+    public static extern IntPtr FindResource(
+      IntPtr instanceHandle,
+      string resource,
+      IntPtr type);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr LoadResource(IntPtr instanceHandle, IntPtr resourceHandle);
+
+    [DllImport("kernel32.dll")]
+    public static extern int SizeofResource(IntPtr instanceHandle, IntPtr resourceHandle);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr LockResource(IntPtr resourceHandle);
+
+    [DllImport("gdi32.dll")]
+    public static extern IntPtr AddFontMemResourceEx(
+      IntPtr fontBuffer,
+      int fontButtonSize,
+      IntPtr reserved,
+      out uint fontsInstalled);
+}
