a00092498a72b837389d0ad7b2979c1dc5139c99
Yoshi Askharoun <jjask7@gmail.com>
2024-03-22 11:49:53 -0500

    Add MenuItemCommand

---
+++ IrisShell/UI/MenuCommands.cs
@@ -0,0 +1,13 @@
+﻿namespace IrisShell.UI;
+
+public class MenuItemCommand : UIXControls.MenuItemCommand
+{
+    public MenuItemCommand()
+    {
+    }
+
+    public MenuItemCommand(string description)
+      : base(description)
+    {
+    }
+}
