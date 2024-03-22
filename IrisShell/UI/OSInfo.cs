de0f3296fcba45b488d86ab3f558eeee4497e4d6
Yoshi Askharoun <jjask7@gmail.com>
2024-03-22 11:50:09 -0500

    Add OSInfo

---
+++ IrisShell/UI/OSInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace IrisShell.UI;
+
+public static class OSInfo
+{
+    private const uint SPI_GETKEYBOARDSPEED = 10;
+    private const uint SPI_GETKEYBOARDDELAY = 22;
+    private static readonly int s_defaultKeyDelay = GetDefaultKeyDelay();
+    private static readonly int s_defaultKeyRepeat = GetDefaultKeyRepeat();
+
+    public static int DefaultKeyDelay => s_defaultKeyDelay;
+
+    public static int DefaultKeyRepeat => s_defaultKeyRepeat;
+
+    private static int GetDefaultKeyDelay()
+    {
+        if (!SystemParametersInfo(SPI_GETKEYBOARDDELAY, 0U, out int pParam, 0))
+            pParam = 1;
+        return (pParam + 1) * 250;
+    }
+
+    private static int GetDefaultKeyRepeat()
+    {
+        if (!SystemParametersInfo(SPI_GETKEYBOARDSPEED, 0U, out int pParam, 0))
+            pParam = 1;
+        return 31000 / (62 + 28 * pParam);
+    }
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool SystemParametersInfo(uint uiAction, uint uiParam, out int pParam, int nWinIni);
+}
