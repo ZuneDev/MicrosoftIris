55815fe59d1473c620da082573bd6656139c0030
Yoshi Askharoun <jjask7@gmail.com>
2025-09-27 02:22:15 -0500

    [WIP] Add debug symbol models

---
+++ UIX/Microsoft/Iris/Debug/Symbols/ApplicationDebugSymbols.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Microsoft.Iris.Debug.Symbols;
+
+public class ApplicationDebugSymbols
+{
+    public List<FileDebugSymbols> Files { get; set; }
+}
+
+public class FileDebugSymbols
+{
+    public string SourceFileName { get; set; }
+
+    public string CompiledFileName { get; set; }
+
+    public Dictionary<uint, ScriptDebugSymbols> ScriptSymbols { get; set; }
+}
+
+public class ScriptDebugSymbols
+{
+    public string SourceCode { get; set; }
+
+    public Dictionary<uint, SourceSpan> SourceMap { get; set; }
+}
+
+[DebuggerDisplay("[{Start}..{End}]")]
+public class SourceSpan
+{
+    public SourceSpan(int start, int end)
+    {
+        if (start > end)
+            throw new ArgumentException($"Source start index must be at or after end index.");
+
+        Start = start;
+        End = end;
+    }
+
+    public int Start { get; set; }
+
+    public int End { get; set; }
+
+    public int GetLength() => End - Start;
+}
+
---
+++ UIX/Microsoft/Iris/Debug/Symbols/DebugSymbolsJsonParser.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Microsoft.Iris.Debug.Symbols;
+
+public static class DebugSymbolsJsonParser
+{
+    public static ApplicationDebugSymbols ParseForApplication(string json) => JsonConvert.DeserializeObject<ApplicationDebugSymbols>(json);
+
+    public static FileDebugSymbols ParseForFile(string json) => JsonConvert.DeserializeObject<FileDebugSymbols>(json);
+
+    public static string Serialize(ApplicationDebugSymbols symbols) => JsonConvert.SerializeObject(symbols);
+
+    public static string Serialize(FileDebugSymbols symbols) => JsonConvert.SerializeObject(symbols);
+}
