651dcaba0ba44e5250d4257aa4ff35f3e873854b
Joshua "Yoshi" Askharoun <jjask7@gmail.com>
2026-07-25 14:02:57 -0500

    Empty UIXrender and UIXsup, retry impl

--- UIXrender/Engine/BufferReceivedHandler.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Microsoft.Iris.Render.Interop;
-
-namespace Microsoft.Iris.Render.Engine;
-
-// A custom (non-generic) delegate so ReadOnlySpan<byte> is legal here -- Span<T> can't
-// be an Action<T>/Func<T> type argument (ref structs aren't valid generic arguments),
-// but a hand-declared delegate type can take one directly.
-public delegate void BufferReceivedHandler(ContextID sourceContext, RENDERHANDLE bufferHandle, BufferFlags flags, ReadOnlySpan<byte> data);
--- UIXrender/Engine/IRenderThreadHandle.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Microsoft.Iris.Render.Interop;
-
-namespace Microsoft.Iris.Render.Engine;
-
-public interface IRenderThreadHandle : IDisposable
-{
-    ContextID ContextId { get; }
-}
